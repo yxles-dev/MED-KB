72cba32fcf6bb9afd18b6ccbbb72800126da8b33
yxles-dev <soriadeniel@gmail.com>
2023-11-20 22:05:57 +0800

    add sfx to login page

--- login.js
+++ login.js
@@ -1,28 +1,37 @@
-var x = document.getElementById("logindiv");
-x.style.display = "none";
-var y = document.getElementById("registerdiv");
-y.style.display = "none";
-
 function logindiv() {
-    console.log("Hello World");
+    const audio = document.getElementById('clickAudio');
+    const audioClone = audio.cloneNode(true);
+    audioClone.play();
     var x = document.getElementById("logindiv");
     if (x.style.display === "none") {
-        console.log("block");
         x.style.display = "block";
     } else {
-        console.log("none");
         x.style.display = "none";
     }
 }
 
 function registerdiv() {
-    console.log("Hello World");
+    const audio = document.getElementById('clickAudio');
+    const audioClone = audio.cloneNode(true);
+    audioClone.play();
     var y = document.getElementById("registerdiv");
     if (y.style.display === "none") {
-        console.log("block");
         y.style.display = "block";
     } else {
-        console.log("none");
         y.style.display = "none";
     }
 }
+
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const audio = document.getElementById('hoverAudio');
+
+button1.addEventListener('mouseover', () => {
+    const audioClone = audio.cloneNode(true);
+    audioClone.play();
+});
+
+button2.addEventListener('mouseover', () => {
+    const audioClone = audio.cloneNode(true);
+    audioClone.play();
+});
